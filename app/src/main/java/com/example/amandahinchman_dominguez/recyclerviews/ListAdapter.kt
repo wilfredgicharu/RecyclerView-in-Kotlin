aff57a8d52dbcd5eec33b84f4ad0d665a5fa11fd
wilfred gicharu <wilfredgicharu254@gmail.com>
2021-11-25 12:02:18 +0300

    displaying images in the recyclerlists

--- app/src/main/java/com/example/amandahinchman_dominguez/recyclerviews/ListAdapter.kt
+++ app/src/main/java/com/example/amandahinchman_dominguez/recyclerviews/ListAdapter.kt
@@ -2,8 +2,13 @@ package com.example.amandahinchman_dominguez.recyclerviews
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import android.graphics.drawable.Drawable
+
+
+
 
 class ListAdapter(private val list: List<Movie>)
     : RecyclerView.Adapter<MovieViewHolder>() {
@@ -26,16 +31,19 @@ class MovieViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
     RecyclerView.ViewHolder(inflater.inflate(R.layout.list_item, parent, false)) {
     private var mTitleView: TextView? = null
     private var mYearView: TextView? = null
+    private var image_view: ImageView? = null
 
 
     init {
         mTitleView = itemView.findViewById(R.id.list_title)
         mYearView = itemView.findViewById(R.id.list_description)
+        image_view= itemView.findViewById(R.id.image_view)
     }
 
     fun bind(movie: Movie) {
         mTitleView?.text = movie.title
         mYearView?.text = movie.year.toString()
+        image_view?.setImageResource(movie.image)
     }
 
 }
